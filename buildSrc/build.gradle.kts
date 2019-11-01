04a492b530059195465b3eaa075a1d6d62ba38a3
Geoffroy Warin <code@geowarin.com>
2019-11-01 21:39:29 +0100

    Cleanup dependencies

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -17,7 +17,13 @@ gradlePlugin {
 }
 
 dependencies {
-  implementation("nu.studer:gradle-jooq-plugin:3.0.3")
-  implementation("org.flywaydb:flyway-gradle-plugin:6.0.7")
+  // Jooq
+  implementation("org.jooq:jooq-codegen:3.12.1")
+  implementation("javax.xml.bind:jaxb-api:2.3.1")
+  implementation("javax.activation:activation:1.1.1")
+  implementation("com.sun.xml.bind:jaxb-core:2.3.0.1")
+  implementation("com.sun.xml.bind:jaxb-impl:2.3.0.1")
+
+  implementation("org.flywaydb:flyway-core:6.0.7")
   implementation("org.jetbrains.kotlin:kotlin-gradle-plugin")
 }
--- buildSrc/src/main/kotlin/DatabaseGenerationPlugin.kt
+++ buildSrc/src/main/kotlin/DatabaseGenerationPlugin.kt
@@ -1,7 +1,5 @@
-import flyway.FlywayTasks
 import flyway.migrationDir
 import jooq.addJooqConfiguration
-import org.flywaydb.core.api.MigrationState
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import java.io.File
@@ -27,28 +25,35 @@ class DatabaseGenerationPlugin : Plugin<Project> {
     val sourceSet = getSourceSet("main")
     sourceSet.java.srcDir { jooqCodegenTargetDirectory }
     sourceSet.kotlin.srcDir { kotlinCodegenTargetDirectory }
-    project.tasks.getByName(sourceSet.compileJavaTaskName).dependsOn += GENERATE_TASK_NAME
-    project.tasks.getByName(sourceSet.getCompileTaskName("kotlin")).dependsOn += KT_CODEGEN_TASK_NAME
 
-    val generateDatabaseTask = project.tasks.create(GENERATE_TASK_NAME, tasks.GenerateDatabaseTask::class.java)
+    val compileJavaTask = project.tasks.getByName(sourceSet.compileJavaTaskName)
+    val compileKotlinTask = project.tasks.getByName(sourceSet.getCompileTaskName("kotlin"))
+
+    val generateDatabaseTask = tasks.create(GENERATE_TASK_NAME, tasks.GenerateDatabaseTask::class.java)
     generateDatabaseTask.jooqClasspath = jooqGradleConfig
     generateDatabaseTask.jooqCodegenTargetDirectory = File(jooqCodegenTargetDirectory)
     generateDatabaseTask.pluginConfig = pluginConfig
     generateDatabaseTask.migrationDir = migrationDir
 
-    generateDatabaseTask.outputs.upToDateWhen {
-      val flywayTasks = FlywayTasks(pluginConfig, jooqGradleConfig, migrationDir)
-      val migrationState = flywayTasks.migrationState()
-      migrationState == MigrationState.SUCCESS
-    }
+//    generateDatabaseTask.outputs.upToDateWhen {
+//      val flywayTasks = FlywayTasks(pluginConfig, jooqGradleConfig, migrationDir)
+//      val migrationState = flywayTasks.migrationState()
+//      migrationState == MigrationState.SUCCESS
+//    }
 
-    val cleanDbTask = project.tasks.create(CLEAN_TASK_NAME, tasks.CleanDbTask::class.java)
+    val cleanDbTask = tasks.create(CLEAN_TASK_NAME, tasks.CleanDbTask::class.java)
     cleanDbTask.jooqClasspath = jooqGradleConfig
     cleanDbTask.pluginConfig = pluginConfig
 
-    val kotlinCodegenTask = project.tasks.create(KT_CODEGEN_TASK_NAME, tasks.KotlinCodegenTask::class.java)
+    val kotlinCodegenTask = tasks.create(KT_CODEGEN_TASK_NAME, tasks.KotlinCodegenTask::class.java)
     kotlinCodegenTask.classpath = codegenConfig
     kotlinCodegenTask.kotlinCodegenTargetDirectory = File(kotlinCodegenTargetDirectory)
     kotlinCodegenTask.pluginConfig = pluginConfig
+
+    compileJavaTask.dependsOn += generateDatabaseTask
+//    kotlinCodegenTask.mustRunAfter(compileJavaTask)
+
+//    kotlinCodegenTask.dependsOn += compileJavaTask
+//    compileKotlinTask.dependsOn += kotlinCodegenTask
   }
 }
