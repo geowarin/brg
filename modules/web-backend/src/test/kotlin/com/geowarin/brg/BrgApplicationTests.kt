bc3f918427e75df45489498773a639a17a170256
Geoffroy Warin <code@geowarin.com>
2019-11-05 23:07:41 +0100

    Improve test

--- modules/web-backend/src/test/kotlin/com/geowarin/brg/BrgApplicationTests.kt
+++ modules/web-backend/src/test/kotlin/com/geowarin/brg/BrgApplicationTests.kt
@@ -1,11 +1,18 @@
 package com.geowarin.brg
 
-import org.assertj.core.api.Assertions.assertThat
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import graphql.ExecutionInput
+import graphql.GraphQL
+import graphql.introspection.IntrospectionQuery
+import graphql.introspection.IntrospectionQuery.INTROSPECTION_QUERY
+import graphql.introspection.IntrospectionResultToSchema
 import org.intellij.lang.annotations.Language
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.skyscreamer.jsonassert.JSONCompare
 import org.skyscreamer.jsonassert.JSONCompareMode
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
 import org.springframework.boot.web.server.LocalServerPort
@@ -14,42 +21,64 @@ import org.springframework.web.reactive.function.client.bodyToMono
 import reactor.kotlin.test.test
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
-class BrgApplicationTests(@LocalServerPort port: Int) {
-    private val client = WebClient.create("http://localhost:$port")
+class BrgApplicationTests(
+  @LocalServerPort port: Int,
+  @Autowired val graphQL: GraphQL
+) {
+  private val client = WebClient.create("http://localhost:$port")
 
-    @Test
-    fun `should handle post`() {
-        client.post().uri("/graphql")
-                .bodyValue(GraphQLParameters(query = "{brg_user{email}}"))
-                .retrieve()
-                .bodyToMono<String>()
-                .test()
-                .consumeNextWith {
-                    it.isJsonEqual("""{
-                                  "data": {
-                                    "brg_user": [
-                                      {
-                                        "email": "tat"
-                                      }
-                                    ]
-                                  }
-                                }
-                     """)
-                }
-                .verifyComplete()
+  @Test
+  fun `should get schema`() {
+    val schemaResult = graphQL.execute(ExecutionInput.newExecutionInput().query(INTROSPECTION_QUERY).build())
+    val schema = IntrospectionResultToSchema().createSchemaDefinition(schemaResult)
+    println(schema)
+  }
+
+  @Test
+  fun `should retrieve users`() {
+    client.graphqlQuery("{brg_user{email}}") {
+      it.isJsonEqual(
+        """
+{
+    "data": {
+      "brg_user": [
+        {
+          "email": "tata"
+        }
+      ]
     }
+  }
+"""
+      )
+    }
+  }
 }
 
-fun String.isJsonEqual(@Language("json") other: String) {
-    val result = JSONCompare.compareJSON(this, other, JSONCompareMode.LENIENT)
-    if (result.failed()) {
-        assertThat(this).isEqualTo(other)
-//        Assertions.assertEquals(other, this)
-    }
+fun WebClient.graphqlQuery(@Language("graphql") query: String, assertion: (String) -> Unit) {
+  this.post().uri("/graphql")
+    .bodyValue(GraphQLParameters(query = query))
+    .retrieve()
+    .bodyToMono<String>()
+    .test()
+    .consumeNextWith(assertion)
+    .verifyComplete()
+}
+
+
+fun String.isJsonEqual(@Language("json") expected: String) {
+  val result = JSONCompare.compareJSON(this, expected, JSONCompareMode.LENIENT)
+  if (result.failed()) {
+    Assertions.assertEquals(prettyJSON(expected), prettyJSON(this))
+  }
+}
+
+fun prettyJSON(jsonObject: String): String {
+  val objectMapper = ObjectMapper()
+  return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(objectMapper.readValue(jsonObject))
 }
 
 data class GraphQLParameters(
-        val query: String
+  val query: String
 )
 
 //data class GraphqlResponse<out T>(
